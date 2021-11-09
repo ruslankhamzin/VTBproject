2ea54c96ff1115b8678e1c17bd72019a1ab87c97
Ruslan Khamzin <hamzin362@gmail.com>
2021-11-09 19:22:34 +0400

    0000h

--- app/src/main/java/ru/startandroid/develop/vtbproject/ui/home/HomeFragment.java
+++ app/src/main/java/ru/startandroid/develop/vtbproject/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package ru.startandroid.develop.vtbproject.ui.home;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,34 +15,48 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 
 import ru.startandroid.develop.vtbproject.R;
+import ru.startandroid.develop.vtbproject.StopypayFragment;
 import ru.startandroid.develop.vtbproject.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(HomeViewModel.class);
-
+        homeViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-
-            }
-        });
+        Button btnStory = (Button) root.findViewById(R.id.story);
+        Button btnaddChild = (Button) root.findViewById(R.id.addChild);
+        btnStory.setOnClickListener(this::onClick);
         return root;
     }
 
+
+
+        @Override
+        public void onDestroyView () {
+            super.onDestroyView();
+            binding = null;
+        }
+
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.story:
+                    Fragment fragment = new StopypayFragment();
+                    FragmentManager fn = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fn.beginTransaction();
+                    fragmentTransaction.replace(R.id.home, fragment);
+                    
+
+                    fragmentTransaction.commit();
+                    break;
+                case R.id.addChild:
+                    break;
+            }
     }
 }
