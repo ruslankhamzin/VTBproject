05a5ed358a5ff453bc3ab2f312089a5987b24f18
Ruslan Khamzin <hamzin362@gmail.com>
2021-11-11 22:59:39 +0400

    small changes

--- app/src/main/java/ru/startandroid/develop/vtbproject/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/ru/startandroid/develop/vtbproject/MyItemRecyclerViewAdapter.java
@@ -32,8 +32,8 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.mIdView.setText(mValues.get(position).shop);
+        holder.mContentView.setText(mValues.get(position).summa);
     }
 
     @Override
